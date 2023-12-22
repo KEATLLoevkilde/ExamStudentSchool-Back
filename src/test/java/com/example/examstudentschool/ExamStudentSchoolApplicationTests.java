30ba2922c6db3fca12cb406422980006ac2d4170
KEATLL <thom4a14@stud.kea.dk>
2023-12-22 13:46:00 +0100

    Changed test to @DataJpaTest

--- src/test/java/com/example/examstudentschool/ExamStudentSchoolApplicationTests.java
+++ src/test/java/com/example/examstudentschool/ExamStudentSchoolApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.examstudentschool;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@DataJpaTest
 class ExamStudentSchoolApplicationTests {
 
     @Test
