0d27f25f39acd2872866660ac7ee914788bde441
KEATLL <thom4a14@stud.kea.dk>
2023-12-22 08:39:37 +0100

    added missing StudentController

---
+++ src/main/java/com/example/examstudentschool/student/controller/StudentController.java
@@ -0,0 +1,42 @@
+package com.example.examstudentschool.student.controller;
+
+import com.example.examstudentschool.student.model.Student;
+import com.example.examstudentschool.student.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable("id") int studentId) {
+        return studentService.getStudentById(studentId);
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.createStudent(student);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable("id") int studentId, @RequestBody Student updatedStudent) {
+        return studentService.updateStudent(studentId, updatedStudent);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable("id") int studentId) {
+        studentService.deleteStudent(studentId);
+    }
+}
